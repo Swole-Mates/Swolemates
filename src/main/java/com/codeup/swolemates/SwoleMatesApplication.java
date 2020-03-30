ce75cbd77bd424e0b6b62d5067ed8f65b860be44
Alexander Bonawitz <Alexander.L.Bonawitz@gmail.com>
2020-03-30 10:50:49 -0500

    first commit

---
+++ src/main/java/com/codeup/swolemates/SwoleMatesApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.swolemates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwoleMatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwoleMatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/swolemates/SwoleMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.swolemates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwoleMatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
