15c5ca5d0587b137911b066e2444ca89f90d10b2
Alexander Bonawitz <Alexander.L.Bonawitz@gmail.com>
2020-04-07 14:16:24 -0500

    Fixed profile and profile edit

--- src/main/java/com/codeup/swolemates/controllers/UserController.java
+++ src/main/java/com/codeup/swolemates/controllers/UserController.java
@@ -3,11 +3,15 @@ package com.codeup.swolemates.controllers;
 
 import com.codeup.swolemates.models.User;
 import com.codeup.swolemates.repositories.UserRepo;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -33,5 +37,34 @@ public class UserController {
         user.setPassword(hash);
         userDao.save(user);
         return "redirect:/login";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        User u = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", u);
+        return "/profile";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable long id) {
+        User u = userDao.getOne(id);
+        model.addAttribute("user", u);
+        return "users/editProfile";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateUser(@ModelAttribute User user, @PathVariable long id) {
+        User updatedUser = userDao.getOne(id);
+        updatedUser.setEmail(user.getEmail());
+        updatedUser.setUsername(user.getUsername());
+        updatedUser.setAge(user.getAge());
+        String hash = passwordEncoder.encode(user.getPassword());
+        updatedUser.setPassword(hash);
+        userDao.save(updatedUser);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(updatedUser, updatedUser.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "redirect:/profile";
     }
 }
